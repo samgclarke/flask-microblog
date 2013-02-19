554d5c839e507b4733a869ba19faff01f3d5d62f
Sam Clarke <cronneloctopus@gmail.com>
2013-02-19 12:03:30 -0800

    Implement OpenID access

--- app/models.py
+++ app/models.py
@@ -12,6 +12,19 @@ class User(db.Model):
     role = db.Column(db.SmallInteger, default=ROLE_USER)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    # speifics for OpenID
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
--- app/views.py
+++ app/views.py
@@ -1,20 +1,35 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, \
+    session, url_for, request, g
+from flask.ext.login import login_user, logout_user, \
+    current_user, login_required
+from app import app, db, lm, old
 from forms import LoginForm
+from models import User, ROLE_USER, ROLE_ADMIN
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'nickname': 'Sam'}  # fake user
+    user = g.user
     # fake array of posts
     posts = [
         {
-            'author': { 'nickname': 'John' },
+            'author': {'nickname': 'John'},
             'body': 'Beautiful day in Portland!'
         },
         {
-            'author': { 'nickname': 'Susan' },
+            'author': {'nickname': 'Susan'},
             'body': 'The Avengers movie was so cool!'
         }
     ]
@@ -27,14 +42,46 @@ def index():
 
 
 @app.route('/login', methods=['GET', 'POST'])
+@old.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
-        return redirect('/index')
+        session['remember_me'] = form.remember_me.data
+        return old.try_login(
+            form.openid.data, ask_for=['nickname', 'email']
+        )
     return render_template(
         'login.html',
         title='Sign In',
         form=form,
         providers=app.config['OPENID_PROVIDERS']
     )
+
+
+@old.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        redirect(url_for('login'))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email=resp.email, role=ROLE_USER)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember=remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
